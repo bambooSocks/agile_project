a11dcc9cb5418dad9686d95ba7e36fec6e286f54
Andreea Teodora Bejenariu <bejenariut@yahoo.com>
2020-03-17 12:14:21 +0100

    Added another feature for M3

--- src/test/java/rcm/M3.java
+++ src/test/java/rcm/M3.java
@@ -1,8 +1,10 @@
 package rcm;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.LinkedList;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,11 +14,13 @@ public class M3 {
 //	private Container container;
 //	private ContainerJourney journey;
 //	private LogisticCompany company;
-	private boolean successful_entry = false;
+//	private Client client;
+//	private boolean successful_entry = false;
+//	private LinkedList<Client> status_list;
 	
 	@Given("a container journey without a container")
 	public void a_container_journey_without_a_container() {
-//	    journey = new Journey(null);
+//	    journey = new ContainerJourney(null);
 	}
 
 	@Given("a container of {string} in a container journey")
@@ -44,7 +48,7 @@ public class M3 {
 	
 	@Then("a journey is successfully updated")
 	public void a_journey_is_successfully_updated() {
-	    assertTrue(successful_entry);
+//	    assertTrue(successful_entry);
 	}
 	
 	@Then("a journey does not contain status {int} degrees, {int} % humidity and {double} bar")
@@ -55,7 +59,39 @@ public class M3 {
 
 	@Then("a journey is failed to update")
 	public void a_journey_is_failed_to_update() {
-		assertFalse(successful_entry);
+//		assertFalse(successful_entry);
+	}
+	
+	
+	/// _2 
+	
+	@Given("a client {string} owns the container journey")
+	public void a_client_owns_the_container_journey(String client_name) {
+//	    client = new Client(client_name);
+//	    client.ownsJourney(journey);
+	}
+
+	@Given("the container journey has a status of {int} degrees, {int} % humidity and {double} bar")
+	public void the_container_journey_has_a_status_of_degrees_humidity_and_bar(Integer temp, Integer humidity, Double pressure) {
+//		ContainerStatus status = new ContainerStatus(temp, humidity, pressure);
+//		successful_entry = company.enterStatus(journey, status);
+		
+	}
+
+	@When("a client requests access to the status")
+	public void a_client_requests_access_to_the_status() {		
+//	    status_list = client.requestStatus();
+	}
+
+	@Then("a list of statuses contains a status of {int} degrees, {int} % humidity and {double} bar")
+	public void a_list_of_statuses_contains_a_status_of_degrees_humidity_and_bar(Integer temp, Integer humidity, Double pressure) {
+//		ContainerStatus status = new ContainerStatus(temp, humidity, pressure);
+//		assertTrue(status_list.contains(status));
+	}
+
+	@Then("a list of statuses is returned")
+	public void a_list_of_statuses_is_returned() {
+//	    assertNotEquals(null, status_list);
 	}
 	
 }
