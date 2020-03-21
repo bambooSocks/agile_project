7f51c76f7ad6a3964ac627e714069d2b4be5e00b
matej <majtan.matej@gmail.com>
2020-03-21 12:22:30 +0100

    added basic attributes and contructor to production classes

--- src/main/java/rcm/LogisticsCompany.java
+++ src/main/java/rcm/LogisticsCompany.java
@@ -1,5 +1,24 @@
 package rcm;
 
+import java.util.LinkedList;
+
 public class LogisticsCompany {
 
+	private int id;
+	private String name;
+	private String address;
+	private String refPerson;
+	private String email;
+	
+	//optional
+	LinkedList<Container> containers;
+
+	public LogisticsCompany(String name, String address, String refPerson, String email) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.refPerson = refPerson;
+		this.email = email;
+	}
+	
 }
