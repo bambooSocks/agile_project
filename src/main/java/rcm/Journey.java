000be029cc07e5dec7fa3039d6f0d1d8f58d83e0
matej <majtan.matej@gmail.com>
2020-03-24 02:18:40 +0100

    ran new formating on the classes

--- src/main/java/rcm/Journey.java
+++ src/main/java/rcm/Journey.java
@@ -3,25 +3,25 @@ package rcm;
 import java.util.LinkedList;
 
 public class Journey {
-	private int id;
-	private String originPort;
-	private String destinationPort;
-	private String content;
-	private String currentPosition;
-	
-	private Container container;
-	private Client client;
-	
-	private LinkedList<ContainerStatus> history;
+    private int id;
+    private String originPort;
+    private String destinationPort;
+    private String content;
+    private String currentPosition;
 
-	public Journey(String originPort, String destinationPort, String content, 
-				   Container container, Client client) {
-		this.originPort = originPort;
-		this.destinationPort = destinationPort;
-		this.content = content;
-		this.container = container;
-		this.client = client;
-	}
-	
-	
+    private Container container;
+    private Client client;
+
+    private LinkedList<ContainerStatus> history;
+
+    public Journey(String originPort, String destinationPort, String content, 
+            Container container, Client client) {
+        this.originPort = originPort;
+        this.destinationPort = destinationPort;
+        this.content = content;
+        this.container = container;
+        this.client = client;
+    }
+
+
 }
