f0c7c4e84b41b5bc56a2bbfb06e4d246c1409b41
matej <majtan.matej@gmail.com>
2020-03-24 11:22:23 +0100

    applied code style on M3.java

--- src/test/java/rcm/M3.java
+++ src/test/java/rcm/M3.java
@@ -17,28 +17,32 @@ public class M3 {
     private LogisticsCompany company1, company2;
     private Client client1, client2;
     private ContainerStatus status;
-    
+
     private boolean successfulEntry = false;
     private LinkedList<Client> statusList;
 
     @Given("a first logistic company {string} with address {string}, reference person {string} and email {string}")
-    public void a_first_logistic_company_with_address_reference_person_and_email(String name, String address , String refPerson, String email) {
-        company1 = new LogisticsCompany( name, address , refPerson, email);
+    public void a_first_logistic_company_with_address_reference_person_and_email(String name, String address,
+            String refPerson, String email) {
+        company1 = new LogisticsCompany(name, address, refPerson, email);
     }
 
     @Given("a second logistic company {string} with address {string}, reference person {string} and email {string}")
-    public void a_second_logistic_company_with_address_reference_person_and_email(String name, String address , String refPerson, String email) {
-        company2 = new LogisticsCompany(name, address , refPerson, email);
+    public void a_second_logistic_company_with_address_reference_person_and_email(String name, String address,
+            String refPerson, String email) {
+        company2 = new LogisticsCompany(name, address, refPerson, email);
     }
 
     @Given("a first client {string} with address {string}, reference person {string} and email {string}")
-    public void a_first_client_with_address_reference_person_and_email(String name, String address , String refPerson, String email) {
-        client1=new Client(name, address , refPerson, email);
+    public void a_first_client_with_address_reference_person_and_email(String name, String address, String refPerson,
+            String email) {
+        client1 = new Client(name, address, refPerson, email);
     }
 
     @Given("a second client {string} with address {string}, reference person {string} and email {string}")
-    public void a_second_client_with_address_reference_person_and_email(String name, String address , String refPerson, String email) {
-        client2 = new Client(name, address , refPerson, email);
+    public void a_second_client_with_address_reference_person_and_email(String name, String address, String refPerson,
+            String email) {
+        client2 = new Client(name, address, refPerson, email);
     }
 
     @Given("a container of first logistic company with ID {int}")
@@ -47,22 +51,26 @@ public class M3 {
     }
 
     @Given("a journey of given container and first client with origin port of {string}, destination port of {string} and a content of {string}")
-    public void a_journey_of_given_container_and_first_client_with_origin_port_of_destination_port_of_and_a_content_of(String originPort, String destinationPort, String content) {
+    public void a_journey_of_given_container_and_first_client_with_origin_port_of_destination_port_of_and_a_content_of(
+            String originPort, String destinationPort, String content) {
         journey = new Journey(originPort, destinationPort, content, container, client1);
     }
-    
+
     @Given("a journey of no container and first client with origin port of {string}, destination port of {string} and a content of {string}")
-    public void a_journey_of_no_container_and_first_client_with_origin_port_of_destination_port_of_and_a_content_of(String originPort, String destinationPort, String content) {
+    public void a_journey_of_no_container_and_first_client_with_origin_port_of_destination_port_of_and_a_content_of(
+            String originPort, String destinationPort, String content) {
         journey = new Journey(originPort, destinationPort, content, null, client1);
     }
 
     @Given("a container status of {double} degrees, {double} % humidity and {double} bar to the given journey")
-    public void a_container_status_of_degrees_humidity_and_bar_to_the_given_journey(Double temperature, Double humidity, Double airPressure) {
+    public void a_container_status_of_degrees_humidity_and_bar_to_the_given_journey(Double temperature, Double humidity,
+            Double airPressure) {
         status = new ContainerStatus(temperature, humidity, airPressure);
     }
 
     @Given("an initial container status in the journey of {double} degrees, {double} % humidity and {double} bar")
-    public void an_initial_container_status_in_the_journey_of_degrees_humidity_and_bar(Double temperature, Double humidity, Double airPressure) {
+    public void an_initial_container_status_in_the_journey_of_degrees_humidity_and_bar(Double temperature,
+            Double humidity, Double airPressure) {
         status = new ContainerStatus(temperature, humidity, airPressure);
         LogisticsCompany company = journey.getCompany();
         successfulEntry = company.enterStatus(journey, status);
@@ -92,7 +100,7 @@ public class M3 {
     public void the_journey_contains_the_given_status() {
         assertTrue(journey.contains(status));
     }
-    
+
     @Then("the journey does not contain the given status")
     public void the_journey_does_not_contain_the_given_status() {
         assertFalse(journey.contains(status));
@@ -107,7 +115,7 @@ public class M3 {
     public void the_journey_has_failed_to_update() {
         assertFalse(successfulEntry);
     }
-    
+
     @Then("a list of statuses is returned")
     public void a_list_of_statuses_is_returned() {
         assertNotEquals(null, statusList);
@@ -117,9 +125,10 @@ public class M3 {
     public void a_list_of_statuses_is_not_returned() {
         assertEquals(null, statusList);
     }
-    
+
     @Then("a list of statuses contains a status of {double} degrees, {double} % humidity and {double} bar")
-    public void a_list_of_statuses_contains_a_status_of_degrees_humidity_and_bar(Double temperature, Double humidity, Double airPressure) {
+    public void a_list_of_statuses_contains_a_status_of_degrees_humidity_and_bar(Double temperature, Double humidity,
+            Double airPressure) {
         ContainerStatus _status = new ContainerStatus(temperature, humidity, airPressure);
         assertTrue(statusList.contains(_status));
     }
