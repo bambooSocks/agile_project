515f8a8e490b3df42e5c868f93acdf6a25cdc086
Jakub Reha <s184478@student.dtu.dk>
2020-03-25 16:06:08 +0100

    Journey ID implemented

--- src/main/java/rcm/Journey.java
+++ src/main/java/rcm/Journey.java
@@ -3,6 +3,7 @@ package rcm;
 import java.util.LinkedList;
 
 public class Journey {
+    private static int idGlobal;
     private int id;
     private String originPort;
     private String destinationPort;
@@ -22,6 +23,25 @@ public class Journey {
         this.container = container;
         this.client = client;
     }
+
+    public void setID() {
+        this.id = idGlobal;
+        idGlobal ++;
+        
+    }
+    
+    public int getID() {
+        return this.id; 
+        
+    }
+    
+    public static void main(String[] args) {
+        Journey j1 = new Journey(null,null,null,null,null);  
+        Journey j2 = new Journey(null,null,null,null,null); 
+        System.out.println(j1.getID());
+        System.out.println(j2.getID());
+        
+    } 
 
 
 }
--- src/test/java/rcm/M2.java
+++ src/test/java/rcm/M2.java
@@ -20,48 +20,48 @@ public class M2 {
     @Given("the first logistics company {string} with address {string}, reference person {string} and email {string}")
     public void the_first_logistics_company_with_address_reference_person_and_email(String name, String address,
             String refPerson, String email) {
-        // logisticsCompany = new LogisticsCompany(name,address,refPerson,email);
+        logisticsCompany = new LogisticsCompany(name,address,refPerson,email);
     }
 
     @Given("the second logistic company {string} with address {string}, reference person {string} and email {string}")
     public void the_second_logistic_company_with_address_reference_person_and_email(String name, String address,
             String refPerson, String email) {
-        // logisticsCompany2 = new LogisticsCompany(name,address,refPerson,email);
+        logisticsCompany2 = new LogisticsCompany(name,address,refPerson,email);
     }
 
     @Given("the container of the first logistics company with ID {int}")
     public void the_container_of_the_first_logistics_company_with_ID(Integer id) {
-        // container = new Container(id, logisticsCompany);
+        container = new Container(id, logisticsCompany);
 
     }
 
     @Given("the first client {string} with address {string}, reference person {string} and email {string}")
     public void the_first_client_with_address_reference_person_and_email(String name, String address, String refPerson,
             String email) {
-        // client = new Client(name,address,refPerson,email);
+        client = new Client(name,address,refPerson,email);
 
     }
 
     @Given("the second client {string} with address {string}, reference person {string} and email {string}")
     public void the_second_client_with_address_reference_person_and_email(String name, String address, String refPerson,
             String email) {
-        // client = new Client(name,address,refPerson,email);
+        client = new Client(name,address,refPerson,email);
     }
 
     @Given("the journey of given container and first client with origin port of {string}, destination port of {string} and a content of {string}")
     public void the_journey_of_given_container_and_first_client_with_origin_port_of_destination_port_of_and_a_content_of(
             String originPort, String destinationPort, String content) {
-        // journey = new Journey(originPort,destinationPort,content,container,client);
+        journey = new Journey(originPort,destinationPort,content,container,client);
     }
 
     @When("the client requests to register the container")
     public void the_client_requests_to_register_the_container() {
-        // journey.setID();
+        journey.setID();
     }
 
     @Then("an Id is created")
     public void an_Id_is_created() {
-        // assertTrue(((client.getContainer()).getJourney()).getID != null);
+        assertTrue(((client.getContainer()).getJourney()).getID != null);
     }
 
     @Given("the container has a location {double} {double}")
@@ -100,7 +100,7 @@ public class M2 {
 
     @When("the first client filters his containers journeys based on the destination {string}")
     public void the_first_client_filters_his_containers_journeys_based_on_the_destination(String destination) {
-        // response = client.filter(destination,journeyList);
+        // response = client.filter(destination);
 
     }
 
