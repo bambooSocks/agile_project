1c3c5043963f093adf188bf66b22e4d8170dce01
Adrienne Cohrt <adriennecohrt@gmail.com>
2020-03-29 13:55:58 +0200

    created abstract User class

--- src/main/java/rcm/Client.java
+++ src/main/java/rcm/Client.java
@@ -2,54 +2,16 @@ package rcm;
 
 import java.util.LinkedList;
 
-public class Client {
-
-    private int id;
-    private String name;
-    private String address;
-    private String refPerson;
-    private String email;
+public class Client extends User {
 
     private LinkedList<Journey> journeyList;
     private LinkedList<Client> clientList;
 
     public Client(String name, String address, String refPerson, String email) {
-        super();
-        this.name = name;
-        this.address = address;
-        this.refPerson = refPerson;
-        this.email = email;
+        super(name, address, refPerson, email);
         clientList = new LinkedList<Client>();
         addProfile(this);
         journeyList = new LinkedList<Journey>();
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + id;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Client other = (Client) obj;
-        if (id != other.id)
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
     }
 
     public void addJourney(Journey journey) {
---
+++ src/main/java/rcm/User.java
@@ -0,0 +1,48 @@
+package rcm;
+
+import java.util.LinkedList;
+
+public abstract class User {
+
+    int id;
+    String name;
+    String address;
+    String refPerson;
+    String email;
+
+    public User(String name, String address, String refPerson, String email) {
+        this.name = name;
+        this.address = address;
+        this.refPerson = refPerson;
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+}
