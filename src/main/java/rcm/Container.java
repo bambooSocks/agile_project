54d92d1abfe1d8a5efe543cb5497e22044837b52
matej <majtan.matej@gmail.com>
2020-03-31 19:55:08 +0200

    Changed a preferred constructor for Container

--- src/main/java/rcm/Container.java
+++ src/main/java/rcm/Container.java
@@ -1,30 +1,34 @@
 package rcm;
 
 public class Container {
-	private int id;
-	private LogisticsCompany company;
-	private Double[] location = new Double[2];
-	
-	public Container(int id, LogisticsCompany company) {
-		this.id = id;
-		this.company = company;
-		company.addToList(this);
-	}
+    private int id;
+    private LogisticsCompany company;
+    private Double[] location = new Double[2];
 
+    @Deprecated
+    public Container(int id, LogisticsCompany company) {
+        this.id = id;
+        this.company = company;
+        company.addToList(this);
+    }
 
-	public LogisticsCompany getCompany() {
-		return company;
-	}
-	
-	public void setLocation(Double x, Double y) {
-		this.location[0] = x;
-		this.location[1] = y;
-	}
+    public Container(LogisticsCompany company) {
+        id = IdGenerator.getInstance().getId(GroupIdType.CONTAINER);
+        this.company = company;
+        company.addToList(this);
+    }
+
+    public LogisticsCompany getCompany() {
+        return company;
+    }
+
+    public void setLocation(Double x, Double y) {
+        this.location[0] = x;
+        this.location[1] = y;
+    }
 
     public int getId() {
         return id;
     }
-
-
 
 }
