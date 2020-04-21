75ecc6dfdc263d86ca3b005254203b2c91800a0a
Matej <majtan.matej@gmail.com>
2020-04-21 23:19:42 +0200

    formatting applied

--- src/test/java/rcm/unitTest/JourneyTest.java
+++ src/test/java/rcm/unitTest/JourneyTest.java
@@ -11,20 +11,20 @@ import rcm.Client;
 import rcm.Journey;
 import rcm.LogisticsCompany;
 
-
 public class JourneyTest {
 
     LogisticsCompany company;
     Client client;
     Journey journey;
-    
+
     @Before
     public void init() {
         company = new LogisticsCompany("Maersk", "Esplanaden 50, 1098 Koebenhavn K", "Soeren Skou", "info@maersk.com");
-        client = company.createClient("Novo Nordisk", "Novo Alle, 2880 Bagsvaerd", "Lars Fruergaard Joergensen", "info@novonordisk.com");
+        client = company.createClient("Novo Nordisk", "Novo Alle, 2880 Bagsvaerd", "Lars Fruergaard Joergensen",
+                "info@novonordisk.com");
         journey = company.createJourney(client, "Rotterdam", "Los Angeles", "tobacco");
     }
-    
+
     @Test
     public void testHashCode() {
         Journey j2 = journey;
