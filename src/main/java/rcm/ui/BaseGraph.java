91c1f3d5f2d80de069f6a0fdb6eb28133c6fccc5
Andreea Teodora Bejenariu <bejenariut@yahoo.com>
2020-04-29 15:30:40 +0200

    changed graphs to support Date

--- src/main/java/rcm/ui/BaseGraph.java
+++ src/main/java/rcm/ui/BaseGraph.java
@@ -1,39 +1,40 @@
-package rcm.ui; 
+package rcm.ui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYDataset;
+
+import rcm.model.Application;
+
+public abstract class BaseGraph extends JPanel {
+    protected Application app;
+
+    public BaseGraph() {
  
-import org.jfree.chart.ChartPanel; 
-import org.jfree.chart.JFreeChart; 
-import org.jfree.data.xy.XYDataset; 
- 
-import javax.swing.BorderFactory; 
-import javax.swing.JPanel; 
- 
-import java.awt.Color; 
-import java.awt.Dimension; 
- 
-public abstract class BaseGraph extends JPanel { 
- 
-    public BaseGraph() { 
- 
-        initUI(); 
- 
-    } 
- 
-    private void initUI() { 
- 
-        XYDataset dataset = createDataset(); 
-        JFreeChart chart = createChart(dataset); 
- 
-        ChartPanel chartPanel = new ChartPanel(chart); 
-        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15)); 
-        chartPanel.setBackground(Color.white); 
-        chartPanel.setPreferredSize(new Dimension(650,400)); 
-        add(chartPanel); 
-    } 
-     
-     
-    
- 
-    public abstract XYDataset createDataset(); 
- 
-    public abstract JFreeChart createChart(XYDataset dataset); 
+        initUI();
+
+    }
+
+    private void initUI() {
+
+        XYDataset dataset = createDataset();
+        JFreeChart chart = createChart(dataset);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        chartPanel.setBackground(new Color(220, 220, 220));
+        chartPanel.setPreferredSize(new Dimension(650, 400));
+        add(chartPanel);
+    }
+
+    public abstract XYDataset createDataset();
+
+    public abstract JFreeChart createChart(XYDataset dataset);
 }
--- src/main/java/rcm/ui/GraphHumidity.java
+++ src/main/java/rcm/ui/GraphHumidity.java
@@ -1,69 +1,69 @@
-package rcm.ui; 
- 
- 
-import org.jfree.chart.JFreeChart; 
-import org.jfree.data.xy.XYDataset; 
- 
-import org.jfree.chart.ChartFactory; 
- 
-import org.jfree.chart.block.BlockBorder; 
-import org.jfree.chart.plot.PlotOrientation; 
-import org.jfree.chart.plot.XYPlot; 
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer; 
-import org.jfree.chart.title.TextTitle; 
- 
-import org.jfree.data.xy.XYSeries; 
-import org.jfree.data.xy.XYSeriesCollection; 
-import java.awt.BasicStroke; 
-import java.awt.Color; 
-import java.awt.Font; 
- 
- 
-public class GraphHumidity extends BaseGraph { 
- 
-    @Override 
-    public XYDataset createDataset() { 
-        XYSeries series = new XYSeries("Humidity"); 
-        series.add(18, 567); 
-        series.add(20, 612); 
-        series.add(25, 800); 
-        series.add(30, 980); 
-        series.add(40, 1410); 
-        series.add(50, 2350); 
- 
-        XYSeriesCollection dataset = new XYSeriesCollection(); 
-        dataset.addSeries(series); 
- 
-        return dataset; 
-    } 
- 
-    @Override 
-    public JFreeChart createChart(XYDataset dataset) { 
-        JFreeChart chart = ChartFactory.createXYLineChart("Himidity of the container during the journey", 
-                "Time (dd/mm/yy hh:mm)", "Humidity (%)", dataset, PlotOrientation.VERTICAL, true, true, false); 
- 
-        XYPlot plot = chart.getXYPlot(); 
- 
-        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(); 
- 
-        renderer.setSeriesStroke(0, new BasicStroke(2.0f)); 
- 
-        plot.setRenderer(renderer); 
-        plot.setBackgroundPaint(Color.white); 
- 
-        plot.setRangeGridlinesVisible(true); 
-        plot.setRangeGridlinePaint(Color.BLACK); 
- 
-        plot.setDomainGridlinesVisible(true); 
-        plot.setDomainGridlinePaint(Color.BLACK); 
- 
-        chart.getLegend().setFrame(BlockBorder.NONE); 
- 
-        renderer.setSeriesPaint(0, Color.CYAN); 
-        chart.setTitle(new TextTitle("Humidity of the container during the journey", 
-                new Font("Serif", java.awt.Font.BOLD, 18))); 
- 
-        return chart; 
-    } 
-     
+package rcm.ui;
+
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.Day;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+
+import org.jfree.chart.ChartFactory;
+
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+
+public class GraphHumidity extends BaseGraph {
+
+    @Override
+    public XYDataset createDataset() {
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        TimeSeries series = new TimeSeries("Humidity");
+
+        series.add(new Day(1, 1, 2017), 56.9);
+        series.add(new Day(2, 1, 2017), 58.9);
+        series.add(new Day(3, 1, 2017), 57.9);
+        series.add(new Day(4, 1, 2017), 57.9);
+        series.add(new Day(5, 1, 2017), 57.3);
+        series.add(new Day(6, 1, 2017), 58.8);
+        series.add(new Day(7, 1, 2017), 57.8);
+
+        dataset.addSeries(series);
+
+        return dataset;
+    }
+
+    @Override
+    public JFreeChart createChart(XYDataset dataset) {
+        JFreeChart chart = ChartFactory.createTimeSeriesChart("Humidity %", "Date", "Humidity",
+                dataset);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.BLACK);
+
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.BLACK);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
+        renderer.setSeriesPaint(0, Color.CYAN);
+        chart.setTitle(new TextTitle("Humidity of the container during the journey",
+                new Font("Serif", java.awt.Font.BOLD, 18)));
+
+        return chart;
+    }
+
 }
