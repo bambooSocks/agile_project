b610d83311ab1e3edfbfc9d1bf9d60f5dd884e8a
Mihaela Nistor <nistor.mihaela5@gmai.com>
2020-05-05 10:05:51 +0200

    Improved coverage for persistency
    + made TestDB2 data more consistent

--- src/test/java/rcm/persistency/TestDB2.java
+++ src/test/java/rcm/persistency/TestDB2.java
@@ -24,22 +24,21 @@ public class TestDB2 {
     }
 
     public static void setupFakeApp(Application app) throws WrongInputException, IOException {
-        app.createNewLogisticsCompany("Agrofert", "Rendebanen 6D, 6000 Kolding", "Peter Hansen",
-                "peter@3plogistics.dk", "Password12345");
-        app.createNewLogisticsCompany("Maersk", "Esplanaden 50, 6000 Kolding", "Cristian Hansen",
-                "christiam@3maersk.dk", "Password12345");
-
-        app.logInUser("peter@3plogistics.dk", "Password12345");
-        app.createNewClient("CBS", "Byhojen 2", "Tom Hanks", "tom@cbs.dk", "Password12345");
-        app.createNewClient("Novozymes", "Smorumvej 43", "William Andersen", "linea@novozymes.dk",
+        app.createNewLogisticsCompany("Agrofert", "Rendebanen 6D, 6000 Kolding", "Peter Hansen", "peter@agrofert.dk",
                 "Password12345");
+        app.createNewLogisticsCompany("Maersk", "Esplanaden 50, 6000 Kolding", "Cristian Hansen",
+                "christian@3maersk.dk", "Password12345");
+
+        app.logInUser("peter@agrofert.dk", "Password12345");
+        app.createNewClient("CBS", "Byhojen 2", "Tom Hanks", "tom@cbs.dk", "Password12345");
+        app.createNewClient("Novozymes", "Smorumvej 43", "William Andersen", "william@novozymes.dk", "Password12345");
         for (int i = 0; i < 8; i++) {
             app.createNewContainer();
         }
 
-        app.logInUser("christiam@3maersk.dk", "Password12345");
-        app.createNewClient("DTU", "Tekniksevej 43", "Pablo Escobar", "linea@dtu.dk", "Password12345");
-        app.createNewClient("Netto", "Ondrovej 43", "Oscar Vinci", "dave@netto.dk", "Password12345");
+        app.logInUser("christian@3maersk.dk", "Password12345");
+        app.createNewClient("DTU", "Tekniksevej 43", "Pablo Escobar", "pablo@dtu.dk", "Password12345");
+        app.createNewClient("Netto", "Ondrovej 43", "Oscar Vinci", "oscar@netto.dk", "Password12345");
         for (int i = 0; i < 7; i++) {
             app.createNewContainer();
         }
@@ -48,47 +47,66 @@ public class TestDB2 {
         Journey j1 = app.requestNewJourney("Copenhagen", "New York", "bananas");
         Journey j3 = app.requestNewJourney("Rotterdam", "London", "cocaine");
 
-        app.logInUser("linea@novozymes.dk", "Password12345");
+        app.logInUser("william@novozymes.dk", "Password12345");
         Journey j2 = app.requestNewJourney("Pearl Harbor", "Tokyo", "robots");
         Journey j4 = app.requestNewJourney("Hong Kong", "New York", "people");
 
-        app.logInUser("linea@dtu.dk", "Password12345");
+        app.logInUser("pablo@dtu.dk", "Password12345");
         Journey j5 = app.requestNewJourney("Los Angeles", "Rio de Janeiro", "furniture");
         Journey j7 = app.requestNewJourney("Rome", "Beijing", "toys");
 
-        app.logInUser("dave@netto.dk", "Password12345");
+        app.logInUser("oscar@netto.dk", "Password12345");
         Journey j6 = app.requestNewJourney("Copenhagen", "Cairo", "cars");
         app.requestNewJourney("New Dili", "New York", "steel");
 
-        app.logInUser("peter@3plogistics.dk", "Password12345");
+        app.logInUser("peter@agrofert.dk", "Password12345");
         app.startJourney(j1.getId(), LocalDateTime.of(2019, 4, 22, 15, 0));
         app.startJourney(j2.getId(), LocalDateTime.of(2019, 4, 23, 15, 0));
         app.startJourney(j3.getId(), LocalDateTime.of(2019, 4, 24, 15, 0));
         app.startJourney(j4.getId(), LocalDateTime.of(2019, 4, 25, 15, 0));
-        app.enterNewContainerStatus(j1.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 22, 15, 0), 35.0, 20.0, 103.0, "Copenhagen"));
-        app.enterNewContainerStatus(j1.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 39.0, 24.0, 102.0, "New York"));
-        app.enterNewContainerStatus(j1.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 24, 15, 0), 12.0, 20.0, 121.0, "Atlantic Ocean"));
-        app.enterNewContainerStatus(j2.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 20, 15, 0), 35.0, 5.0, 90.0, "Pacific Ocean"));
-        app.enterNewContainerStatus(j3.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 30, 15, 0), 0.0, 89.0, 101.0, "Indian Ocean"));
-        app.enterNewContainerStatus(j3.getId(), new ContainerStatus(LocalDateTime.of(2020, 5, 22, 19, 32), -35.0, 20.0, 101.0, "San Diego"));
-        app.enterNewContainerStatus(j4.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 22, 17, 0), -35.0, 2.0, 101.0, "Wakanda"));
-        app.enterNewContainerStatus(j4.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 53.0, 2.0, 101.0, "Atlantida"));
-        app.enterNewContainerStatus(j4.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 24, 15, 0), 35.0, 12.0, 132.0, "Copenhagen"));
+        app.enterNewContainerStatus(j1.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 22, 15, 0), 35.0, 20.0, 103.0, "Copenhagen"));
+        app.enterNewContainerStatus(j1.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 39.0, 24.0, 102.0, "New York"));
+        app.enterNewContainerStatus(j1.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 24, 15, 0), 12.0, 20.0, 121.0, "Atlantic Ocean"));
+        app.enterNewContainerStatus(j2.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 20, 15, 0), 35.0, 5.0, 90.0, "Pacific Ocean"));
+        app.enterNewContainerStatus(j3.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 30, 15, 0), 0.0, 89.0, 101.0, "Indian Ocean"));
+        app.enterNewContainerStatus(j3.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 5, 22, 19, 32), -35.0, 20.0, 101.0, "San Diego"));
+        app.enterNewContainerStatus(j4.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 22, 17, 0), -35.0, 2.0, 101.0, "Wakanda"));
+        app.enterNewContainerStatus(j4.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 53.0, 2.0, 101.0, "Atlantida"));
+        app.enterNewContainerStatus(j4.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 24, 15, 0), 35.0, 12.0, 132.0, "Copenhagen"));
 
-        app.logInUser("christiam@3maersk.dk", "Password12345");
+        app.logInUser("christian@3maersk.dk", "Password12345");
         app.startJourney(j5.getId(), LocalDateTime.of(2019, 4, 26, 15, 0));
         app.startJourney(j6.getId(), LocalDateTime.of(2019, 4, 28, 15, 0));
         app.startJourney(j7.getId(), LocalDateTime.of(2019, 4, 28, 15, 0));
-        app.enterNewContainerStatus(j5.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 05, 15, 0), 35.0, 20.0, 91.0, "Prague"));
-        app.enterNewContainerStatus(j5.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 45.0, 20.0, 101.0, "Moscow"));
-        app.enterNewContainerStatus(j5.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 27, 15, 0), 3.0, 32.0, 121.0, "Red Sea"));
-        app.enterNewContainerStatus(j7.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 5.0, 20.0, 111.0, "Dead Sea"));
-        app.enterNewContainerStatus(j7.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 24, 15, 0), 35.0, 24.0, 111.0, "Black Sea"));
-        app.enterNewContainerStatus(j7.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 25, 15, 0), 35.0, 20.0, 108.0, "Copenhagen"));
-        app.enterNewContainerStatus(j7.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 26, 15, 0), 35.0, 24.0, 101.0, "North Pole"));
-        app.enterNewContainerStatus(j7.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 27, 15, 0), 35.0, 20.0, 121.0, "Antarctida"));
-        app.enterNewContainerStatus(j6.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 22, 15, 0), 35.0, 6.0, 106.0, "Copenhagen"));
-        app.enterNewContainerStatus(j6.getId(), new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 35.0, 5.0, 107.0, "Copenhagen"));
+        app.enterNewContainerStatus(j5.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 05, 15, 0), 35.0, 20.0, 91.0, "Prague"));
+        app.enterNewContainerStatus(j5.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 45.0, 20.0, 101.0, "Moscow"));
+        app.enterNewContainerStatus(j5.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 27, 15, 0), 3.0, 32.0, 121.0, "Red Sea"));
+        app.enterNewContainerStatus(j7.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 5.0, 20.0, 111.0, "Dead Sea"));
+        app.enterNewContainerStatus(j7.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 24, 15, 0), 35.0, 24.0, 111.0, "Black Sea"));
+        app.enterNewContainerStatus(j7.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 25, 15, 0), 35.0, 20.0, 108.0, "Copenhagen"));
+        app.enterNewContainerStatus(j7.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 26, 15, 0), 35.0, 24.0, 101.0, "North Pole"));
+        app.enterNewContainerStatus(j7.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 27, 15, 0), 35.0, 20.0, 121.0, "Antarctida"));
+        app.enterNewContainerStatus(j6.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 22, 15, 0), 35.0, 6.0, 106.0, "Copenhagen"));
+        app.enterNewContainerStatus(j6.getId(),
+                new ContainerStatus(LocalDateTime.of(2020, 4, 23, 15, 0), 35.0, 5.0, 107.0, "Copenhagen"));
     }
-    
+
 }
--- src/test/java/rcm/persistency/TestDatabaseAccess.java
+++ src/test/java/rcm/persistency/TestDatabaseAccess.java
@@ -45,9 +45,10 @@ public class TestDatabaseAccess {
         cl2 = app.createNewClient("Novo Nordisk", "Lyngbyvej 56", "Linea Hansen", "linea@novo.dk", "Password12345");
         c1 = app.createNewContainer();
         app.logInUser("tom@dtu.dk", "Tom123456");
-        timestamp = LocalDateTime.of(2019, 4, 22, 15, 0);
+        timestamp = LocalDateTime.of(2020, 4, 16, 15, 0);
         j1 = app.requestNewJourney("Copenhagen", "New York", "robots");
-
+        app.logInUser("peter@maersk.dk", "Password12345");
+        app.startJourney(j1.getId(), timestamp);
         timestamp2 = LocalDateTime.of(2020, 4, 22, 15, 0);
         cs1 = new ContainerStatus(timestamp2, 35.0, 20.0, 101.0, "Copenhagen");
     }
@@ -57,22 +58,20 @@ public class TestDatabaseAccess {
 
         app.logInUser("peter@maersk.dk", "Password12345");
         boolean success = app.enterNewContainerStatus(j1.getId(), cs1);
-
+        assertTrue(success);
         repo = new SqliteRepository();
 
         Container dbContainer = repo.readContainer(c1.getId());
+        assertEquals(c1.getId(), dbContainer.getId());
         dbClient = repo.readClient(cl1.getEmail());
         LogisticsCompany dbUser3 = repo.readLogisticsCompany(lc1.getEmail());
-        Journey dbJourney = repo.readJourney(j1.getId());
-        // TODO: group2 do you need this mili variable?
-        long mili = new Date().getTime();
-        ContainerStatus dbStatus = dbJourney.getStatus().get(0);
         assertEquals("Maersk", dbUser3.getName());
-        assertEquals("DTU", dbClient.getName());
-        assertEquals(c1.getId(), dbContainer.getId());
+        Journey dbJourney = repo.readJourney(j1.getId());
+
         assertEquals("robots", dbJourney.getContent());
+        ContainerStatus dbStatus = dbJourney.getStatus().get(0);
         assertEquals(35.0, dbStatus.getTemperature(), 0.001);
-        assertTrue(success);
+
     }
 
     @Test
@@ -82,21 +81,19 @@ public class TestDatabaseAccess {
         repo.updateCompany(lc1);
         assertEquals("client@maersk.dk", repo.readClient(cl1.getEmail()).getEmail());
     }
-    
-    
+
     @Test
     public void testreadAllCompanies() throws IOException, WrongInputException {
-        LogisticsCompany lc2 = new LogisticsCompany("Doprava", "Jedlova 21", "Petr Zeleny", "zelda@novo.dk", "Password12345");
-        List <LogisticsCompany> list = repo.readAllLogisticsCompanies();
-        assertTrue(list.contains(lc2)&&list.contains(lc1));
+        LogisticsCompany lc2 = app.createNewLogisticsCompany("Doprava", "Jedlova 21", "Petr Zeleny", "zelda@novo.dk",
+                "Password12345");
+        List<LogisticsCompany> list = repo.readAllLogisticsCompanies();
+        assertTrue(list.contains(lc2) && list.contains(lc1));
     }
-    
-    
-    
+
     @Test
     public void testSharedJourney() throws IOException, WrongInputException {
         Client cl3 = app.createNewClient("Brambora", "Lesova 3", "Linea Hansen", "linea2@novo.dk", "Password12345");
-        cl1.shareJourney(cl2,j1);
+        cl1.shareJourney(cl2, j1);
         assertTrue(cl2.getSharedJourneyList().contains(j1));
         assertFalse(cl3.getSharedJourneyList().contains(j1));
     }
